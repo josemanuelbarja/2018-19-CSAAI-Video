0a80b49b917f1a4737c95aa2bae3bd9f9e9c980e
José Manuel Barja Vázquez <josemanuelbarja@MacBook-Pro-de-Jose-5.local>
2019-03-28 10:46:48 +0100

    Creo javascript y css

---
+++ mytv_prog.js
@@ -0,0 +1,46 @@
+function main()
+{
+  console.log("TV_MAIN")
+
+  //-- Obtener parrafo
+  parrafo = document.getElementById('parrafo')
+  //-- Obtener videos
+  video1 = document.getElementById("video1")
+  video2 = document.getElementById("video2")
+  video3 = document.getElementById("video3")
+  //-- Boton de Play
+  ver1 = document.getElementById('play1')
+  ver2 = document.getElementById('play2')
+  ver3 = document.getElementById('play3')
+  //-- Establecer el tamaño de videos
+  video1.width = 400;
+  video1.height= 200;
+
+  video2.width = 400;
+  video2.height= 200;
+
+  video3.width = 400;
+  video3.height= 200;
+
+
+  //-- Detectar pasada por encima
+  parrafo.onmouseover = () => {
+    console.log("Mouse over!!")
+  }
+  //-- Al apretar el boton de ver video
+  ver1.onclick = () => {
+    console.log("Play1")
+    //-- Indicar la fuente del vídeo
+    video1.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
+  }
+  ver2.onclick = () => {
+    console.log("Play2")
+    //-- Indicar la fuente del vídeo
+    video2.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
+  }
+  ver3.onclick = () => {
+    console.log("Play2")
+    //-- Indicar la fuente del vídeo
+    video3.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
+  }
+}
